d30a908848c506863301123927c38c4239f3dad6
rijinraju <rijinraju856@gmail.com>
2022-10-03 17:05:28 +0530

    settings is modified 3

--- BeeShopee/settings.py
+++ BeeShopee/settings.py
@@ -21,7 +21,7 @@ import dj_database_url
 
 
 from django.core.exceptions import ImproperlyConfigured
-from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
