d96b486ac2274da75583bfce786fd354db868564
rijinraju <rijinraju856@gmail.com>
2022-10-04 09:09:30 +0530

    setting sec_key in  settings secret key

--- BeeShopee/settings.py
+++ BeeShopee/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-SECRET_KEY = 'django-insecure-3g%f!eiikn&s8!hjg4_w3(lw)-8hvk9u-bpfm*t5ngt1#!)#kl'
+SECRET_KEY = config("SEC_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool,default=False)
